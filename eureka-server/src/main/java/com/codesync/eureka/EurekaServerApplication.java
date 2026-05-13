dd5de4dae25476023584d3852bf20382dea64ebd
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-05-13 21:02:16 +0530

    [Anjali] : Implemented Eureka discovery server

---
+++ eureka-server/src/main/java/com/codesync/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.codesync.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * CodeSync Eureka Discovery Server.
+ * All microservices register here; the API Gateway uses it for routing.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
